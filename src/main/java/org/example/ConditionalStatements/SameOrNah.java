c56939722fd2e7f6368e8b741eb058573d2058b3
Patrick Xiong <PXstiegler@gmail.com>
2024-07-23 16:08:11 -0400

    Done

--- src/main/java/org/example/ConditionalStatements/SameOrNah.java
+++ src/main/java/org/example/ConditionalStatements/SameOrNah.java
@@ -1,4 +1,19 @@
 package org.example.ConditionalStatements;
+import java.util.Scanner;
 
 public class SameOrNah {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = sc.nextLine();
+
+        System.out.println("Enter another word: ");
+        String otherWord = sc.nextLine();
+
+        if (word.equalsIgnoreCase(otherWord)) {
+            System.out.println("The words are the same");
+        } else {
+            System.out.println("The words are different");
+        }
+    }
 }
--- src/main/java/org/example/ConditionalStatements/TestScores.java
+++ src/main/java/org/example/ConditionalStatements/TestScores.java
@@ -4,16 +4,33 @@ import java.util.Scanner;
 public class TestScores {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a word:");
-        String word = sc.nextLine();
 
-        System.out.println("Enter another word: ");
-        String otherWord = sc.nextLine();
+        // Prompt the user to enter a number between 0 and 100
+        System.out.println("Name a number between 0 and 100");
+        int score = sc.nextInt();
 
-        if (word.equalsIgnoreCase(otherWord)) {
-            System.out.println("The words are the same");
+        // Determine the grade based on the score
+        String grade;
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80) {
+            grade = "B";
+        } else if (score >= 70) {
+            grade = "C";
+        } else if (score >= 60) {
+            grade = "D";
         } else {
-            System.out.println("The words are different");
+            grade = "F";
         }
+
+        // Print the grade
+        System.out.println("Your grade is a " + grade);
+
+        // Optionally, add an emoticon for an F grade
+        if (grade.equals("F")) {
+            System.out.println("Your grade is an F :(");
+        }
+
+        sc.close();
     }
 }
